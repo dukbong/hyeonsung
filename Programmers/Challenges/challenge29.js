0ce7ce8fa9d701272e84dacfec4438b9290b9e19
HyeonSung Jang <gorae930301@gmail.com>
2022-07-01 21:25:02 +0900

    nodejs

--- Programmers/Challenges/challenge29.js
+++ Programmers/Challenges/challenge29.js
@@ -6,7 +6,7 @@ let info = [
   "python frontend senior chicken 150",
   "cpp backend senior pizza 260",
   "java backend junior chicken 80",
-  "python backend senior chicken 50",
+  "python backend senior chicken 50"
 ];
 let query = [
   "java and backend and junior and pizza 100",
@@ -14,49 +14,30 @@ let query = [
   "cpp and - and senior and pizza 250",
   "- and backend and senior and - 150",
   "- and - and - and chicken 100",
-  "- and - and - and - 150",
+  "- and - and - and - 150"
 ];
 
 function solution(info, query) {
-    let answer = [];
-    info = info.map((v) => v.split(/\s/g));
-    query = query.map((v) => v.split(/\s/g).filter((v) => v != "and"));
-    let check = query.map((v) => v.filter((v) => v != "-")); // 조건 갯수를 구하기 위해...ㅠ
-  
-    for (let i = 0; i < query.length; i++) {
-      let result = [];
-      let last; // 객체로 만들어버리겠다!
-      let cnt = 0;
-      for (let j = 0; j < query[i].length; j++) {
-          for (let k = 0; k < info.length; k++) {
-            if (j === query[i].length - 1){
-            Number(info[k][j]) >= Number(query[i][j]) &&result.push(info[k].join(""));
-            // info[k][info[k].length-1] 대신 info[k].at(-1)을 쓸 수 있는데...
-            // 프로그래머스에서 아직은 안된다.
-          }else{
-            for (let k = 0; k < info.length; k++) {
-                info[k].includes(query[i][j]) && result.push(info[k].join(""));
-              }
+  let answer = [];
+  info = info.map((v) => v.split(/\s/g));
+  query = query.map((v) => v.replace(/-/g, "").split(/\s/g).filter((v) => v != "and"));
+
+  for (let i = 0; i < query.length; i++) {
+    let last = 0;
+    for (let j = 0; j < info.length; j++) {
+      let check = 0;
+      for (let k = 0; k < info[j].length; k++) {
+        if (k === info[j].length - 1) {
+          Number(info[j][k]) >= Number(query[i][k]) && last++;
+        } else {
+          if (!info[j][k].includes(query[i][k])) {
+            break;
           }
         }
       }
-      last = result.reduce((acc, cur) => {
-        if (!acc[cur]) {
-          acc[cur] = 1;
-        } else {
-          acc[cur]++;
-        }
-        return acc;
-      }, {});
-  
-      for (let key in last) {
-        const value = last[key];
-        if(value === check[i].length){
-          cnt ++;
-        }
-      }
-      answer.push(cnt);
     }
-    return answer;
+    answer.push(last);
   }
+  return answer;
+}
 console.log(solution(info, query));
