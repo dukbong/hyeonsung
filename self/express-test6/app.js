03dd4514b448e7031fa80d9899988d916cf8679f
HyeonSung Jang <gorae930301@gmail.com>
2022-07-07 02:06:15 +0900

    experss-study

---
+++ self/express-test6/app.js
@@ -0,0 +1,56 @@
+const express       = require("express"),
+      morgan        = require("morgan"),
+      session       = require("express-session"),
+      cookieParser  = require("cookie-parser"),
+      dotenv        = require("dotenv"),
+      nunjucks      = require("nunjucks"),
+      {sequelize}   = require("./models/index"),
+      app           = express();
+
+dotenv.config();
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+    express : app,
+    watch   : true,
+});
+
+sequelize.sync({force : true})
+    .then(() => {
+        console.log("MySQL Connection Success!");
+    }).catch((err) => {
+        console.error(err);
+    });
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser(process.env.COOKIE));
+
+app.use(session({
+    resave            : false,
+    saveUninitialized : false,
+    secret            : process.env.COOKIE,
+    cookie : {
+        httpOnly : true,
+        secure   : true,
+    },
+}));
+
+app.use((req, res, next) => {
+    const err = new Error(`${req.method} ${req.url} No route was found here!`);
+    err.status(400);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message  =  err.message;
+    res.locals.error    =  process.env.ERROR !== "production" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+app.listen(app.set("port"), () => {
+    console.log(`${app.set("port")} waiting on Server!`);
+});
---
+++ self/express-test6/models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model{
+    static init (sequelize){
+        super.init({
+            email : {
+                type:Sequelize.STRING(50),
+                allowNull : false,
+                unique : true,
+            },
+            password : {
+                type : Sequelize.STRING(100),
+                allowNull : false,
+            },
+            address : {
+                type : Sequelize.TEXT,
+                allowNull : false,
+            },
+            nick : {
+                type : Sequelize.STRING(10),
+                allowNull : false,
+            },
+            lang : {
+                type : Sequelize.STRING(20),
+                allowNull : false,
+            },
+            info : {
+                type: Sequelize.STRING(100),
+            }
+        },{
+            sequelize,
+            underscored : false,
+            timestamps : true,
+            paranoid : true,
+            modelName : "User",
+            tableName : "express6",
+            charset : "utf8",
+            collate : "utf8_general_ci",
+        });
+    }
+}
